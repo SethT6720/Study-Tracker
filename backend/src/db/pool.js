4723b190c74a81bf43ad546f90686165f37f8c54
Seth Tanner <101337407+SethT6720@users.noreply.github.com>
2026-02-26 19:20:22 +0000

    getting ready for deployment

--- backend/src/db/pool.js
+++ backend/src/db/pool.js
@@ -1,5 +1,8 @@
 const pg = require('pg');
 
-const pool = new pg.Pool();
+const pool = new pg.Pool({
+    connectionString: process.env.DATABASE_URL || undefined,
+    ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
+});
 
 module.exports = pool;
